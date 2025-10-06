926d4c27438a8f403d4ca7b8f59d904c01277468
Roberto Moyano <robertocarlosmoyano@gmail.com>
2025-10-06 19:54:49 +0200

    feat: actualizar estilos y lógica del componente de tarjeta del clima para reflejar condiciones meteorológicas más precisas

--- src/app/component/weather-card/weather-card.component.ts
+++ src/app/component/weather-card/weather-card.component.ts
@@ -19,7 +19,12 @@ export class WeatherCardComponent {
 
   ngOnInit(): void {
     this.obtenerFechaActual();
-    this.setWeatherStyleAndIcon();
+  }
+
+  ngOnChanges(): void {
+    if (this.city) {
+      this.setWeatherStyle(this.city.weather);
+    }
   }
 
   obtenerFechaActual(): void {
@@ -34,33 +39,60 @@ export class WeatherCardComponent {
 
   }
 
-  setWeatherStyleAndIcon(): void {
-    switch (this.city.weather.toLowerCase()) {
-      case 'sunny':
-        this.weatherIcon = 'bi-sun-fill';
-        this.weatherIconColor = 'text-warning';
-        this.weatherStyle =  'background-sunny';
-        break;
-      case 'cloudy':
-        this.weatherIcon = 'bi-cloud-fill';
-        this.weatherIconColor = 'text-secondary';
-        this.weatherStyle = 'background-cloudy';
-        break;
-      case 'rainy':
-        this.weatherIcon = 'bi-cloud-drizzle';
-        this.weatherIconColor = 'text-primary';
-        this.weatherStyle = 'background-rainy';
-        break;
-      case 'snowy':
-        this.weatherIcon = 'bi-snow';
-        this.weatherIconColor = 'text-light';
-        this.weatherStyle = 'background-snowy';
-        break;
-      default:
-        this.weatherIcon = 'bi-question-circle';
-        this.weatherIconColor = 'text-warning';
-        this.weatherStyle = 'background-sunny';
-        break;
+  setWeatherStyle(weather: string) {
+    const condition = weather.toLowerCase();
+
+    if (condition.includes('clear')) {
+      this.weatherStyle = 'background-clear';
+      this.weatherIcon = 'bi-sun-fill';
+      this.weatherIconColor = 'text-clear';
+    } else if (condition.includes('cloud')) {
+      this.weatherStyle = 'background-clouds';
+      this.weatherIcon = 'bi-cloud-fill';
+      this.weatherIconColor = 'text-clouds';
+    } else if (condition.includes('rain')) {
+      this.weatherStyle = 'background-rain';
+      this.weatherIcon = 'bi-cloud-rain-fill';
+      this.weatherIconColor = 'text-rain';
+    } else if (condition.includes('drizzle')) {
+      this.weatherStyle = 'background-drizzle';
+      this.weatherIcon = 'bi-cloud-drizzle-fill';
+      this.weatherIconColor = 'text-drizzle';
+    } else if (condition.includes('thunderstorm')) {
+      this.weatherStyle = 'background-thunderstorm';
+      this.weatherIcon = 'bi-cloud-lightning-fill';
+      this.weatherIconColor = 'text-thunderstorm';
+    } else if (condition.includes('snow')) {
+      this.weatherStyle = 'background-snow';
+      this.weatherIcon = 'bi-snow-fill';
+      this.weatherIconColor = 'text-snow';
+    } else if (condition.includes('mist') || condition.includes('fog') || condition.includes('haze')) {
+      this.weatherStyle = 'background-mist';
+      this.weatherIcon = 'bi-cloud-fog-fill';
+      this.weatherIconColor = 'text-mist';
+    } else if (
+      condition.includes('smoke') ||
+      condition.includes('dust') ||
+      condition.includes('sand') ||
+      condition.includes('ash')
+    ) {
+      this.weatherStyle = 'background-smoke';
+      this.weatherIcon = 'bi-cloud-smoke-fill';
+      this.weatherIconColor = 'text-smoke';
+    } else if (condition.includes('squall')) {
+      this.weatherStyle = 'background-squall';
+      this.weatherIcon = 'bi-cloud-squall-fill';
+      this.weatherIconColor = 'text-squall';
+    } else if (condition.includes('tornado')) {
+      this.weatherStyle = 'background-tornado';
+      this.weatherIcon = 'bi-tornado';
+      this.weatherIconColor = 'text-tornado';
+    } else {
+      // por defecto
+      this.weatherStyle = 'background-clear';
+      this.weatherIcon = 'bi-sun-fill';
+      this.weatherIconColor = 'text-clear';
     }
   }
+
 }
--- src/app/service/weather.service.ts
+++ src/app/service/weather.service.ts
@@ -6,11 +6,11 @@ import { City } from '../models/city.model';
 })
 export class WeatherService {
   private cities: City[] = [
-    { name: 'Granada', countryIso: 'ES', temperature: 24, weather: 'Sunny', humidity: 30, wind: 10 },
-    { name: 'Madrid', countryIso: 'ES', temperature: 22, weather: 'Cloudy', humidity: 50, wind: 15 },
-    { name: 'Barcelona', countryIso: 'ES', temperature: 20, weather: 'Rainy', humidity: 70, wind: 20 },
-    { name: 'Valencia', countryIso: 'ES', temperature: 26, weather: 'Sunny', humidity: 40, wind: 12 },
-    { name: 'Sevilla', countryIso: 'ES', temperature: 28, weather: 'Sunny', humidity: 35, wind: 8 }
+    { name: 'Granada', countryIso: 'ES', temperature: 24, weather: 'Clear', humidity: 30, wind: 10 },
+    { name: 'Madrid', countryIso: 'ES', temperature: 22, weather: 'Cloud', humidity: 50, wind: 15 },
+    { name: 'Barcelona', countryIso: 'ES', temperature: 20, weather: 'Rain', humidity: 70, wind: 20 },
+    { name: 'Valencia', countryIso: 'ES', temperature: 26, weather: 'Clear', humidity: 40, wind: 12 },
+    { name: 'Sevilla', countryIso: 'ES', temperature: 28, weather: 'Clear', humidity: 35, wind: 8 }
   ];
 
   constructor() {
